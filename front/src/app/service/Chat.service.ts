da88a0aed9ba9f645ac3c8f11096361f322a175b
rory <35566455+Thorekt@users.noreply.github.com>
2026-02-16 22:49:50 +0100

    feat: add récupération de l'historique coté front

--- front/src/app/service/Chat.service.ts
+++ front/src/app/service/Chat.service.ts
@@ -2,6 +2,7 @@ import { Injectable, OnDestroy } from '@angular/core';
 import { Client, StompSubscription } from '@stomp/stompjs';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import Message from '../type/Message.type';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -11,15 +12,19 @@ import Message from '../type/Message.type';
     }
 )
 class ChatService implements OnDestroy {
+    private readonly API_URL = 'http://localhost:8080/api/room';
     private readonly WS_URL = 'ws://localhost:8080/ws';
     private readonly TOPIC_PREFIX = '/topic/room.';
 
-    private client?: Client;
+
+    private stompClient?: Client;
+    
     private subscription?: StompSubscription;
 
     private connected$ = new BehaviorSubject<boolean>(false);
     private messages$ = new Subject<Message>();
 
+    constructor(private httpClient: HttpClient) {}
     
     isConnected(): Observable<boolean>  {
         return this.connected$.asObservable();
@@ -30,24 +35,41 @@ class ChatService implements OnDestroy {
     }
 
     connect(roomName: string): void {
-        if(this.client?.active) {
+        if(this.stompClient?.active) {
             console.warn('Already connected to WebSocket');
             return;
         }
+        this.retrieveHistory(roomName);        
+        this.connectStompClient(roomName);        
+    }
 
-        this.client = new Client({
+    retrieveHistory(roomName: string): void {
+        this.httpClient.get<Message[]>(this.API_URL + '/' + roomName )
+            .subscribe({
+                next: (messages) => {
+                    messages.forEach(message => this.messages$.next(message));
+                },
+                error: (err) => {
+                    console.error('Failed to retrieve message history:', err);
+                }
+            });
+    }
+
+    connectStompClient(roomName: string): void {
+        this.stompClient = new Client({
             brokerURL: this.WS_URL,
             reconnectDelay: 5000,
             debug: (str) => console.log(str)
         });
 
-        this.client.onConnect = () => this.onConnect(roomName);
-        this.client.activate();
+        this.stompClient.onConnect = () => this.onConnect(roomName);
+        this.stompClient.activate();
     }
+    
     
     onConnect(roomName: string): void {
         this.connected$.next(true);
-        this.subscription = this.client?.subscribe(this.TOPIC_PREFIX + roomName, (message) => {
+        this.subscription = this.stompClient?.subscribe(this.TOPIC_PREFIX + roomName, (message) => {
             const chatMessage: Message = JSON.parse(message.body);
             this.messages$.next(chatMessage);
         });
@@ -56,17 +78,17 @@ class ChatService implements OnDestroy {
     disconnect(): void {
         this.subscription?.unsubscribe();
         this.subscription = undefined;
-        this.client?.deactivate();
-        this.client = undefined;
+        this.stompClient?.deactivate();
+        this.stompClient = undefined;
         this.connected$.next(false);
     }
 
     sendMessage(message: Message): void {
-        if(!this.client?.active) {
+        if(!this.stompClient?.active) {
             console.warn('Cannot send message, WebSocket is not connected');
             return;
         }
-        this.client.publish({
+        this.stompClient.publish({
             destination: '/app/chat.send',
             body: JSON.stringify(message)
         });
